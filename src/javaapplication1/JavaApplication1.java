71bedcb193815ee367ac3b42c4701f6770298e55
AlbertoJM <leojmtest@gmail.com>
2019-02-19 10:14:50 -0600

    suma de sos numeros

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -4,22 +4,39 @@
  * and open the template in the editor.
  */
 package javaapplication1;
+import java.util.Scanner;
 
 /**
  *
  * @author AJM
  */
+//https://www.youtube.com/watch?v=fqTGczXq-kk
+        //Trabajo Colaborativo con Netbeans y GitHub
 public class JavaApplication1 {
 
     /**
      * @param args the command line arguments
      */
+   
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("mi primer repositorio en netbeas");
+        // TODO code application logic here 
+        Scanner dato = new Scanner(System.in);
+        int a;
+        int b;
+        int suma;
+       
+        System.out.println("Escribe el primer numero");
+        a = dato.nextInt();
         
-        //https://www.youtube.com/watch?v=fqTGczXq-kk
-        //Trabajo Colaborativo con Netbeans y GitHub
+        System.out.println("Dame el segundo Número");
+        b = dato.nextInt();
+        suma=a+b;
+        System.out.println ( "Este es el resultado:"+"\n"+suma) ;
+        
+        
+        
+        
     }
     
 }
